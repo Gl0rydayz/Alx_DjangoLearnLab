5bbc1016eec2a0acb2ac5d1975cccb6ae402aca2
Oussama IOUIRY <121458608+Gl0rydayz@users.noreply.github.com>
2025-09-14 19:58:16 +0100

    moved files

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import permission_required
-from django.http import HttpResponse
 from .models import Book
+from .forms import BookForm
 
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
-    books = Book.objects.all()
+def book_search(request):
+    query = request.GET.get('q', '')
+    if query:
+        # Use ORM to prevent SQL injection
+        books = Book.objects.filter(title__icontains=query)
+    else:
+        books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
-@permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
     if request.method == 'POST':
-        # Logic to create a book
-        return HttpResponse("Book created!")
-    return render(request, 'bookshelf/book_form.html')
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()  # Safe handling of user input
+            return render(request, 'bookshelf/success.html')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/book_form.html', {'form': form})
 
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def book_edit(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        # Logic to edit the book
-        return HttpResponse("Book updated!")
-    return render(request, 'bookshelf/book_form.html', {'book': book})
+MIDDLEWARE += [
+    'csp.middleware.CSPMiddleware',
+]
 
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def book_delete(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    book.delete()
-    return HttpResponse("Book deleted!")
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'", "'unsafe-inline'")
+CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
